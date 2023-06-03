2a3d5c5530f5892a3f2d6b46f503d4c37d5978c6
twaldorf <trevor.automated@gmail.com>
2023-06-03 15:38:49 -0700

    move value from header to implementation

--- src/texture.hpp
+++ src/texture.hpp
@@ -35,13 +35,7 @@ public:
     checker_texture(colour c1, colour c2)
             : even(make_shared<solid_colour>(c1)) , odd(make_shared<solid_colour>(c2)) {}
 
-    virtual colour value(double u, double v, const point3& p) const override {
-        auto sines = sin(10*p.x())*sin(10*p.y())*sin(10*p.z());
-        if (sines < 0)
-            return odd->value(u, v, p);
-        else
-            return even->value(u, v, p);
-    }
+    virtual colour value(double u, double v, const point3& p) const override;
 
 public:
     shared_ptr<texture> odd;
